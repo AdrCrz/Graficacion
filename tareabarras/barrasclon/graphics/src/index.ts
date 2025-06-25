a420a4f41964ee0c63327ffb2acd6380ebd797b7
Adriel Cruz <73452941+AdrCrz@users.noreply.github.com>
2025-06-25 12:13:02 -0600

    Update index.ts

--- tareabarras/barrasclon/graphics/src/index.ts
+++ tareabarras/barrasclon/graphics/src/index.ts
@@ -6,6 +6,21 @@ let graphics: CanvasRenderingContext2D;
 canvas = <HTMLCanvasElement>document.getElementById('circlechart');
 graphics = canvas.getContext('2d');
 
-const miCanvas:CanvasLocal = new CanvasLocal(graphics, canvas);
+const miCanvas: CanvasLocal = new CanvasLocal(graphics, canvas);
 
-miCanvas.paint();
+function getHValues(): number[] {
+    const inputs = document.querySelectorAll('#h-inputs input');
+    return Array.from(inputs).map(input => {
+        let val = parseFloat(input.value);
+        return (val >= 10 && val <= 100) ? val : 0; 
+    });
+}
+function draw(): void {
+    graphics.clearRect(0, 0, canvas.width, canvas.height); // Clear canvas
+    miCanvas.paint(getHValues());
+}
+const inputElements = document.querySelectorAll('#h-inputs input');
+inputElements.forEach(input => {
+    input.addEventListener('input', draw);
+});
+draw();
